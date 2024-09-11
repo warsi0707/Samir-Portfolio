f9ddc1eca962a3d464c7fc7e022c5a38fbcd141c
Samir Warsi <warsisamir78.com>
2024-09-11 15:27:19 +0530

    new changes

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Project() {
   return (
     <>
@@ -8,7 +6,7 @@ export default function Project() {
           My Recent <strong className="text-purple-500">Works</strong>
         </h1>
         <p className="text-xl">
-          Here are a few projects i've worked on recently
+          Here are a few projects I have worked on recently
         </p>
       </div>
       <div className="grid grid-cols-1  px-2 gap-10 justify-items-center mx-24  md:grid-cols-2 lg:grid-cols-3">
@@ -39,33 +37,6 @@ export default function Project() {
 
         <div className="w-72 h-[550px] border-2 border-sky-500 rounded-xl p-4 md:w-full my-10">
           <img
-            src="https://plus.unsplash.com/premium_photo-1683309568772-57011d6c1b7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG9kb3xlbnwwfHwwfHx8MA%3D%3D"
-            alt=""
-            className="w-full h-[200px] rounded-xl"
-          />
-          <h1 className="text-2xl text-center py-2">To-Do</h1>
-          <p className="text-lg text-center">
-            To rember your daily tasks you can use this app, Basically it stored
-            your data of task and once have you done your task you can mark as
-            done or also you can delete your task as well as you can add new
-            tasks.
-          </p>
-          <div className="btn flex justify-center gap-5 my-10 text-white ">
-            <a href="https://github.com/warsi0707/ReactToDo">
-              <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
-                Github
-              </button>
-            </a>
-            <a href="#">
-              <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
-                Live
-              </button>
-            </a>
-          </div>
-        </div>
-
-        <div className="w-72 h-[550px] border-2 border-sky-500 rounded-xl p-4 md:w-full my-10">
-          <img
             src="https://plus.unsplash.com/premium_photo-1666739387996-2a45b0a5dab7?q=80&w=1953&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
             className="w-full h-[200px] rounded-xl"
@@ -76,12 +47,38 @@ export default function Project() {
             city name.
           </p>
           <div className="btn flex justify-center gap-5 my-10 text-white ">
-            <a href="https://github.com/warsi0707/IpFinder">
+            <a href="https://github.com/warsi0707/IpAddress">
               <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
                 Github
               </button>
             </a>
-            <a href="#">
+            <a href="https://ip-address-123.netlify.app/">
+              <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
+                Live
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div className="w-72 h-[550px] border-2 border-sky-500 rounded-xl p-4 md:w-full my-10">
+          <img
+            src="https://images.unsplash.com/photo-1574265932292-3d3d43b3c893?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className="w-full h-[200px] rounded-xl"
+          />
+          <h1 className="text-2xl text-center py-2">
+            Body Mass Index calculator
+          </h1>
+          <p className="text-lg text-center">
+            BMI calculator gives your body index with your height and weight
+          </p>
+          <div className="btn flex justify-center gap-5 my-10 text-white ">
+            <a href="https://github.com/warsi0707/BMIcalculator">
+              <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
+                Github
+              </button>
+            </a>
+            <a href="https://bmi-calc-123.netlify.app/">
               <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
                 Live
               </button>
@@ -115,21 +112,24 @@ export default function Project() {
 
         <div className="w-72 h-[550px] border-2 border-sky-500 rounded-xl p-4 md:w-full my-10">
           <img
-            src="https://images.unsplash.com/photo-1574265932292-3d3d43b3c893?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://plus.unsplash.com/premium_photo-1683309568772-57011d6c1b7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG9kb3xlbnwwfHwwfHx8MA%3D%3D"
             alt=""
             className="w-full h-[200px] rounded-xl"
           />
-          <h1 className="text-2xl text-center py-2">Body Mass Index calculator</h1>
+          <h1 className="text-2xl text-center py-2">To-Do</h1>
           <p className="text-lg text-center">
-            BMI calculator gives your body index with your height and weight
+            To rember your daily tasks you can use this app, Basically it stored
+            your data of task and once have you done your task you can mark as
+            done or also you can delete your task as well as you can add new
+            tasks.
           </p>
           <div className="btn flex justify-center gap-5 my-10 text-white ">
-            <a href="https://github.com/warsi0707/BMIcalculator">
+            <a href="https://github.com/warsi0707/ReactToDo">
               <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
                 Github
               </button>
             </a>
-            <a href="https://bmi-calc-123.netlify.app/">
+            <a href="#">
               <button className="w-28 h-10 text-lg bg-purple-600 rounded-md transition ease-in-out delay-100 hover:translate-1 hover:scale-110">
                 Live
               </button>
